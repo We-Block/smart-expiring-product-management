05a8a6a448df8dc0da1d44aebfc50972478c38a8
“Last-emo-boygit config --global user.email chenyuqian0122@outlook.com” <chenyuqian0122@outlook.com>
2023-07-22 17:02:48 +0800

    update

--- contracts/DiscountLib.sol
+++ contracts/DiscountLib.sol
@@ -1,23 +1,48 @@
 // SPDX-License-Identifier: MIT
+/**
+ * @title Discount Library
+ * @author w33d
+ * @notice Library for update status of discount
+ * @dev This library contains several functions to update status of discount and calculate discounted price
+ */
 pragma solidity ^0.8.0;
 
 library DiscountLib {
+
+    /**
+    * @dev Discount data storage struct 
+    */
     struct DiscountData {
         bool isActive;
         uint256 percentage;
     }
 
+    /**
+    * @dev Sets a discount percentage
+    * @param self DiscountData storage pointer
+    * @param percentage Discount percentage to set
+    */
     function setDiscount(DiscountData storage self, uint256 percentage) public {
         require(percentage > 0 && percentage < 100, "Invalid discount percentage");
         self.isActive = true;
         self.percentage = percentage;
     }
 
+    /**
+    * @dev Cancels any active discount
+    * @param self DiscountData storage pointer
+    */
     function cancelDiscount(DiscountData storage self) public {
         self.isActive = false;
         self.percentage = 0;
     }
 
+    /**
+    * @dev Calculates discounted price based on original price and active discount
+    * @param self DiscountData storage pointer
+    * @param originalPrice Original non-discounted price
+    * @return Discounted price
+    */
     function calculateDiscountedPrice(DiscountData storage self, uint256 originalPrice) public view returns (uint256) {
         if (self.isActive) {
             return originalPrice * (100 - self.percentage) / 100;
--- contracts/PriceLib.sol
+++ contracts/PriceLib.sol
@@ -1,9 +1,18 @@
 // SPDX-License-Identifier: MIT
+/**
+ * @title Price Library
+ * @author w33d
+ * @notice Library for calculating price
+ * @dev This library contains fucntions used to calculate price and update price
+ */
 pragma solidity ^0.8.0;
 
 import "./ProductLib.sol";
 
 library PriceLib {
+    /**
+     * @dev Constants for product price tiers based on days to expiry
+     */
     uint256 constant THIRTY_DAYS = 30;
     uint256 constant SEVEN_DAYS = 7;
     uint256 constant PRICE_30_DAYS = 1000;
@@ -11,6 +20,11 @@ library PriceLib {
     uint256 constant PRICE_LESS_7_DAYS = 500;
     uint256 constant SECONDS_IN_DAY = 86400;
 
+    /**
+     * @dev Calculates price based on number of days until product expiry.
+     * @param productExpiryDate Expiry date of the product
+     * @return The calculated price
+     */
     function calculatePrice(uint256 productExpiryDate) internal view returns (uint256) {
         require(productExpiryDate > block.timestamp, "Expiry date must be in the future");
 
@@ -19,6 +33,11 @@ library PriceLib {
         return remainingDays > THIRTY_DAYS ? PRICE_30_DAYS : (remainingDays > SEVEN_DAYS ? PRICE_7_DAYS : PRICE_LESS_7_DAYS);
     }
 
+    /**
+    * @dev Updates the price of a product by calculating the new price based on expiry date
+    * @param product The Product struct to update
+    * @return The newly calculated price
+    */
     function updatePrice(ProductLib.Product storage product) internal returns (uint256) {
         product.price = calculatePrice(product.expiryDate);
         return product.price;
